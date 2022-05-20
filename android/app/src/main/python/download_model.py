e412df2414a878cfc847a1553e395f7d9dc17f15
Bitlabs Software <105653526+bitlabs-software@users.noreply.github.com>
2022-05-20 20:27:34 +0200

    Update download_model.py

--- android/app/src/main/python/download_model.py
+++ android/app/src/main/python/download_model.py
@@ -6,7 +6,7 @@ from os.path import dirname, join
 import os
 
 def download():
-    url = "https://github.com/angmolin/Android-LiveChess2FEN/releases/download/base/Xception.tflite"
+    url = "https://github.com/bitlabs-software/Squarevision/releases/download/v1.0.0/Xception.tflite"
     local_filename = join(os.environ["HOME"], "Xception.tflite")
     if not os.path.exists(local_filename):
         with requests.get(url, stream=True) as r:
